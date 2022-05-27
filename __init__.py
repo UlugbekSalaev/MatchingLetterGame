d70364659a607687ef10c3bb00d68b30029d99e9
ulugbek0302 <ulugbek0302@gmail.com>
2022-05-27 18:39:06 +0500

    changing

--- __init__.py
+++ __init__.py
@@ -1,23 +1,48 @@
 # yumshatish va ayirish belsini chiqarib tashlanadi
 # 27 ta harf
+from nltk import ngrams
+from itertools import combinations
+import operator
 
 letters = ['а', 'b', 'd', 'е', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'о', 'p', 'q', 'r', 's', 't',
-           'u', 'v', 'x', 'y', 'ḡ', 'ō', 'ş', 'ç']
+           'u', 'v', 'x', 'y', 'ḡ', 'ō', 'c']  # 'ş', 'ç'
 
-from itertools import combinations
-
-lst = ["a", "b", "c", "d"]
-
-k = 0
 cubics = []
 for i in combinations(letters, 6):
     cubics.append(i)
 print(len(cubics))
 
-k=0
+k = 0
 for i in combinations(cubics, 8):
     print(i)
-    k+=1
-    if k==1:
-         break;
+    k += 1
+    if k == 1:
+        break;
 print(k)
+freq = {}
+words = []
+with open("words", encoding="utf8") as file:
+    lines = file.readlines()
+words = [line.rstrip() for line in lines]
+
+for word in words:
+    bigrams = ngrams(word, 2)
+
+    for grams in bigrams:
+        print(grams[0]+grams[1])
+        key = grams[0]+grams[1]
+        if key in freq:
+            freq[key] += 1
+        else:
+            freq[key] = 1
+s_freq = dict( sorted(freq.items(), key=operator.itemgetter(1), reverse=True))
+print(s_freq)
+
+cubics = []
+with open("cubics", encoding="utf8") as file:
+    lines = file.readlines()
+    cubics = [line.rstrip().split() for line in lines]
+
+for word in words:
+    shu kubiklarga nicha suz yasash mumkinligni hisobla chiq,
+    keyin yana boshqa yul bn kubik yasa, hisobla
