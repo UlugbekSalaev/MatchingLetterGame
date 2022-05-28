9fd9c8e69fb91080df204f7f3c46f3fbf7ca6ef8
ulugbek0302 <ulugbek0302@gmail.com>
2022-05-28 13:47:55 +0500

    changing

--- __init__.py
+++ __init__.py
@@ -2,47 +2,81 @@
 # 27 ta harf
 from nltk import ngrams
 from itertools import combinations
+from itertools import permutations
 import operator
 
-letters = ['а', 'b', 'd', 'е', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'о', 'p', 'q', 'r', 's', 't',
-           'u', 'v', 'x', 'y', 'ḡ', 'ō', 'c']  # 'ş', 'ç'
-
+letters = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ō', 'p', 'q', 'r', 's', 'c', 't',
+           'u', 'v', 'x', 'y', 'z', 'ḡ']  # 'ş', 'ç'
+print(len(letters))
 cubics = []
 for i in combinations(letters, 6):
     cubics.append(i)
-print(len(cubics))
+print("cubics count = " + str(len(cubics)))
 
-k = 0
 for i in combinations(cubics, 8):
     print(i)
-    k += 1
-    if k == 1:
-        break;
-print(k)
-freq = {}
+    break
+
 words = []
+letter_frq = {}
 with open("words", encoding="utf8") as file:
     lines = file.readlines()
 words = [line.rstrip() for line in lines]
 
+bi_freq = {}
 for word in words:
-    bigrams = ngrams(word, 2)
-
-    for grams in bigrams:
-        print(grams[0]+grams[1])
-        key = grams[0]+grams[1]
-        if key in freq:
-            freq[key] += 1
+    for c in word:
+        if c in letter_frq:
+            letter_frq[c] += 1
         else:
-            freq[key] = 1
-s_freq = dict( sorted(freq.items(), key=operator.itemgetter(1), reverse=True))
-print(s_freq)
+            letter_frq[c] = 1
+
+    bigrams = ngrams(word, 2)
+    for grams in bigrams: # grams = ('z', 'u')
+        key = grams[0]+grams[1]
+        if key in bi_freq:
+            bi_freq[key] += 1
+        else:
+            bi_freq[key] = 1
+
+letter_frq = dict(sorted(letter_frq.items(), key=operator.itemgetter(1), reverse=True))
+bi_freq = dict( sorted(bi_freq.items(), key=operator.itemgetter(1), reverse=True))
+print(letter_frq)
+print(bi_freq)
+for i in letter_frq:
+    if i not in letters:
+        print("Errors by mismatch between word letter and letters letter" + i)
 
 cubics = []
-with open("cubics", encoding="utf8") as file:
+with open("cubics", encoding="utf8") as file: # one row is cubic's letter
     lines = file.readlines()
     cubics = [line.rstrip().split() for line in lines]
 
+card_words = {}
+card_words_cnt = {}
+
+def is_exist(word:str, n:int, cubic: tuple):
+    for i in range(n):
+        if word[i] not in cubic[i]:
+            return False
+    return True
+
 for word in words:
-    shu kubiklarga nicha suz yasash mumkinligni hisobla chiq,
-    keyin yana boshqa yul bn kubik yasa, hisobla
+    #shu kubiklarga nicha suz yasash mumkinligni hisobla chiq,
+    #keyin yana boshqa yul bn kubik yasa, hisobla
+    n = len(word)  # word's length
+    for i in permutations(cubics, n):
+        if is_exist(word, n, i):
+            if word in card_words_cnt:
+                card_words_cnt[word] += 1
+            else:
+                card_words_cnt[word] = 1
+                card_words[word] = i
+
+card_words_cnt = dict(sorted(card_words_cnt.items(), key=operator.itemgetter(1), reverse=True))
+ss = 0
+with open("card_words", "w", encoding="utf8") as file:
+    for i in card_words_cnt:
+        file.writelines(i + "," + str(card_words_cnt[i]) + "\n")
+        ss += card_words_cnt[i]
+print(ss)
