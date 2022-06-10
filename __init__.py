7289196a7425d00c79f540357c976a6b6dffa6fa
ulugbek0302 <ulugbek0302@gmail.com>
2022-06-10 15:13:04 +0500

    Developed

--- __init__.py
+++ __init__.py
@@ -7,13 +7,15 @@ import operator
 
 letters = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ō', 'p', 'q', 'r', 's', 'c', 't',
            'u', 'v', 'x', 'y', 'z', 'ḡ']  # 'ş', 'ç'
+cc = 7  # cubic count
 print(len(letters))
 cubics = []
 for i in combinations(letters, 6):
     cubics.append(i)
-print("cubics count = " + str(len(cubics)))
-
-for i in combinations(cubics, 8):
+print("Count of all available cubics generated from letters = " + str(len(cubics)))
+#print(cubics[0])
+print("-------Print one combination for game's 8 cubics-------")
+for i in combinations(cubics, cc):
     print(i)
     break
 
@@ -40,19 +42,60 @@ for word in words:
             bi_freq[key] = 1
 
 letter_frq = dict(sorted(letter_frq.items(), key=operator.itemgetter(1), reverse=True))
-bi_freq = dict( sorted(bi_freq.items(), key=operator.itemgetter(1), reverse=True))
+bi_freq = dict(sorted(bi_freq.items(), key=operator.itemgetter(1), reverse=True))
 print(letter_frq)
-print(bi_freq)
+print(dict(list(bi_freq.items())[0:120]))
+
+my_letters = letters.copy()
+last_bi = ""
+ind = 0
+for i in bi_freq:
+    if i[0] in my_letters:
+        my_letters.remove(i[0])
+        if not my_letters:
+            last_bi = i
+            break
+    if i[1] in my_letters:
+        my_letters.remove(i[1])
+        if not my_letters:
+            last_bi = i
+            break
+    ind += 1
+print(last_bi)
+print(ind)
+
+cubletter = ""
+dubl = ['a','i','o','r','l','s','u','t','n','m','k','q','b','h','y']
+new_bi_freq = dict(list(bi_freq.items())[0:120])
+for i in new_bi_freq:
+    if i[0] not in cubletter:
+        cubletter += i[0]
+    else:
+        if i[0] in dubl:
+            cubletter += i[0]
+            dubl.remove(i[0])
+    if i[1] not in cubletter:
+        cubletter += i[1]
+    else:
+        if i[1] in dubl:
+            cubletter += i[1]
+            dubl.remove(i[1])
+
+for i in range(7):
+    for j in range(7):
+        print(cubletter[i*6+j], end='\t')
+    print()
+
+# checking errors in words' letter
 for i in letter_frq:
     if i not in letters:
-        print("Errors by mismatch between word letter and letters letter" + i)
+        print("Errors by mismatch between word letter and letters letter " + i)
 
 cubics = []
-with open("cubics", encoding="utf8") as file: # one row is cubic's letter
+with open("cubics"+str(cc), encoding="utf8") as file: # one row is cubic's letter
     lines = file.readlines()
     cubics = [line.rstrip().split() for line in lines]
 
-card_words = {}
 card_words_cnt = {}
 
 def is_exist(word:str, n:int, cubic: tuple):
@@ -71,12 +114,8 @@ for word in words:
                 card_words_cnt[word] += 1
             else:
                 card_words_cnt[word] = 1
-                card_words[word] = i
 
 card_words_cnt = dict(sorted(card_words_cnt.items(), key=operator.itemgetter(1), reverse=True))
-ss = 0
-with open("card_words", "w", encoding="utf8") as file:
+with open("card_words"+str(cc), "w", encoding="utf8") as file:
     for i in card_words_cnt:
         file.writelines(i + "," + str(card_words_cnt[i]) + "\n")
-        ss += card_words_cnt[i]
-print(ss)
