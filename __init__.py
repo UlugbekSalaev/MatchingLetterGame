cb79204a3a91a975a2daa033e7998159d37a898d
UlugbekSalaev <ulugbek0302@gmail.com>
2022-09-06 23:23:23 +0500

    Algorithms developed, English dataset added

--- __init__.py
+++ __init__.py
@@ -6,7 +6,18 @@ import operator
 
 letters = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ō', 'p', 'q', 'r', 's', 'c', 't',
            'u', 'v', 'x', 'y', 'z', 'ḡ']  # 'ş', 'ç'
-cc = 5  # cubic count
+
+soft = ['a', 'i', 'o', 'u', 'e', 'ō',]
+hard = ['r', 'l', 's', 't', 'n', 'm', 'q', 'k', 'y', 'h', 'b', 'd', 'z', 'v', 'p', 'f', 'g', 'j', 'ḡ', 'x', 'c']
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+           'u', 'v', 'w', 'x', 'y', 'z']  #  english
+soft = ['a', 'i', 'o', 'u', 'e']
+hard = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't',
+           'v', 'w', 'x', 'y', 'z']
+
+cc = 8  # cubic count
 lc = len(letters) # length of alphabet
 print("Len of the alphabet="+str(len(letters)))
 cubics = []
@@ -14,13 +25,13 @@ for i in combinations(letters, 6):
     cubics.append(i)
 print("Count of all available cubics generated from letters = " + str(len(cubics)))
 #print(cubics[0])
-print("-------Print one combination for game's "+str(cc)+" cubics-------")
-for i in combinations(cubics, cc):
-    print(i)
-    break
+# print("-------Print one combination for game's "+str(cc)+" cubics-------")
+# for i in combinations(cubics, cc):
+#     print(i)
+#     break
 
 words = []
-with open("words", encoding="utf8") as file:
+with open("words_eng", encoding="utf8") as file:
     lines = file.readlines()
 words = [line.rstrip() for line in lines]
 
@@ -80,31 +91,64 @@ print(last_bi_ind)
 
 new_bi_freq = dict(list(bi_freq.items())[0:last_bi_ind])
 print(new_bi_freq)
-print(tri_freq)
+# print(tri_freq)
 
 cubletter = ""
 dubl = []
 if cc*6 - lc > 0:
-    dubl = list(letter_frq)[:cc*6-lc]
+    if cc*6 - lc <= len(soft):
+        dubl = soft[:cc*6-lc]
+    else:
+        dubl = soft + list(hard)[:cc*6-lc-len(soft)]
+    # dubl = list(letter_frq)[:cc*6-lc]
 print("dubl="+",".join(dubl))
 # dubl = []
 new_bi_freq = dict(list(bi_freq.items())[0:last_bi_ind])
+new_dbl = [] # sorted by bigram order
+
 for i in new_bi_freq:
     if i[0] not in cubletter:
         cubletter += i[0]
         if i[0] in dubl:
-            cubletter += i[0]
+            # cubletter += i[0]
             dubl.remove(i[0])
+            new_dbl.append(i[0])
     if i[1] not in cubletter:
         cubletter += i[1]
         if i[1] in dubl:
-            cubletter += i[1]
+            # cubletter += i[1]
             dubl.remove(i[1])
-
+            new_dbl.append(i[1])
 print(cubletter)
+print(new_dbl)
+
+# for i in range(cc):
+#     for j in range(6):
+#         print(cubletter[j*cc+i], end='\t')
+#     print()
+
+cubes =[]
+cubes = [[] for i in range(cc)]
+cnt=0
+for j in range(6):
+    for i in range(cc):
+        if len(cubletter)>cnt:
+            cubes[i].append(cubletter[cnt])
+            cnt += 1
+
+cnt1 = 0
+di = 0
+for i in range(6):
+    for j in range(cc):
+        if cnt1 >= cnt:
+            # if cubletter[cnt1] in cubes[j]
+            cubes[j].append(new_dbl[di])
+            di += 1
+        cnt1 += 1
+
 for i in range(cc):
     for j in range(6):
-        print(cubletter[j*cc+i], end='\t')
+        print(cubes[i][j], end='\t')
     print()
 
 # checking errors in words' letter
--- statistics.py
+++ statistics.py
@@ -1,16 +1,28 @@
 from itertools import permutations
 import operator
-
-cc = 7
+soft = ['a', 'i', 'o', 'u', 'e', 'ō',]
+cc = 8
 cubics = []
 words = []
-with open("words", encoding="utf8") as file:
+with open("words_eng", encoding="utf8") as file:
     lines = file.readlines()
 words = [line.rstrip() for line in lines]
 
 with open("cubics"+str(cc), encoding="utf8") as file: # one row is cubic's letter
     lines = file.readlines()
     cubics = [line.rstrip().split() for line in lines]
+
+# checking restrictions
+for i in cubics:
+    ss = set(i)
+    if len(ss) != len(i):
+        print("there is dublicate in cubic", i)
+    sc = 0
+    for j in i:
+        if i in soft:
+            sc += 1
+    if sc>2:
+        print("There is more vovel in cubic:", i)
 
 card_words_cnt = {}
 
@@ -35,3 +47,32 @@ card_words_cnt = dict(sorted(card_words_cnt.items(), key=operator.itemgetter(1),
 with open("card_words"+str(cc), "w", encoding="utf8") as file:
     for i in card_words_cnt:
         file.writelines(i + "," + str(card_words_cnt[i]) + "\n")
+
+# exit()
+# calculate information
+t3 = 518
+t4 = 1165
+t5 = 2875
+
+t3 = 1026
+t4 = 2499
+t5 = 2499
+
+print('#', '\t', t3+t4+t5, '\t', t3, '\t', t4, '\t', t5, '\t')
+for ind in range(4,9):
+    with open("card_words"+str(ind), encoding="utf8") as file: #
+        lines = file.readlines()
+        words_c = [line.rstrip().split(',')[0] for line in lines]
+
+    cnt3=0
+    cnt4=0
+    cnt5=0
+    for i in words_c:
+        if len(i) == 3:
+            cnt3 += 1
+        if len(i) == 4:
+            cnt4 += 1
+        if len(i) == 5:
+            cnt5 += 1
+    print(ind, '\t', len(words_c), '\t', cnt3, '\t', cnt4, '\t', cnt5, '\t')
+    print(ind, '\t', round(len(words_c)/(t3+t4+t5),3), '\t', round(cnt3/t3,3), '\t', round(cnt4/t4,3), '\t', round(cnt5/t5,3), '\t')
