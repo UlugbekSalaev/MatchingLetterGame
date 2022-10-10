841cb4b187691697446af7848f5553c00cf01dc1
ulugbek0302 <ulugbek0302@gmail.com>
2022-10-10 13:46:35 +0500

    Fixed

---
+++ testing_rnd.py
@@ -0,0 +1,48 @@
+from itertools import permutations
+import operator
+
+dataset = "en"
+
+if dataset == "uz":
+    soft = ['a', 'i', 'o', 'u', 'e', 'ō']    # uz
+else:
+    soft = ['e', 'a', 'o', 'i', 'u']      # en
+cc = 8
+cubics = []
+words = []
+
+def is_exist(word:str, n:int, cubic: tuple):
+    for i in range(n):
+        if word[i] not in cubic[i]:
+            return False
+    return True
+
+for app in range(2):
+    for iteration in range(1):
+        print("App=", app, " iteration=", iteration)
+
+        with open("words_"+dataset, encoding="utf8") as file:
+            lines = file.readlines()
+        words = [line.rstrip() for line in lines]
+
+        with open("result/testrnd_"+dataset+"/train_res_app"+str(app), encoding="utf8") as file: # one row is cubic's letter
+            lines = file.readlines()
+            cubics = [line.rstrip().split() for line in lines]
+
+        card_words_cnt = {}
+
+        for word in words:
+            #shu kubiklarga nicha suz yasash mumkinligni hisobla chiq,
+            #keyin yana boshqa yul bn kubik yasa, hisobla
+            n = len(word)  # word's length
+            for i in permutations(cubics, n):
+                if is_exist(word, n, i):
+                    if word in card_words_cnt:
+                        card_words_cnt[word] += 1
+                    else:
+                        card_words_cnt[word] = 1
+
+        card_words_cnt = dict(sorted(card_words_cnt.items(), key=operator.itemgetter(1), reverse=True))
+        with open("result/testrnd_"+dataset+"/test_res_app"+str(app), "w", encoding="utf8") as file:
+            for i in card_words_cnt:
+                file.writelines(i + "," + str(card_words_cnt[i]) + "\n")
---
+++ testingrnd_l.py
@@ -0,0 +1,34 @@
+cc = 8  # cubes count
+for app in range(2):
+    for iteration in range(1):
+
+        with open("words_en", encoding="utf8") as file:   #
+            lines = file.readlines()
+            words_c = [line.rstrip().split(',')[0] for line in lines]
+        t3 = 0
+        t4 = 0
+        t5 = 0
+        for i in words_c:
+            if len(i) == 3:
+                t3 += 1
+            if len(i) == 4:
+                t4 += 1
+            if len(i) == 5:
+                t5 += 1
+
+        with open("result/testrnd_en/test_res_app"+str(app), encoding="utf8") as file: #
+            lines = file.readlines()
+            words_c = [line.rstrip().split(',')[0] for line in lines]
+        cnt3 = 0
+        cnt4 = 0
+        cnt5 = 0
+        for i in words_c:
+            if len(i) == 3:
+                cnt3 += 1
+            if len(i) == 4:
+                cnt4 += 1
+            if len(i) == 5:
+                cnt5 += 1
+        print(app, '\t', iteration, '\t', (t3+t4+t5), '\t', t3, '\t', t4, '\t', t5, '\t')
+        print(app, '\t', iteration, '\t', (cnt3+cnt4+cnt5), '\t', cnt3, '\t', cnt4, '\t', cnt5, '\t')
+        print(app, '\t', iteration, '\t', round(len(words_c) / (t3 + t4 + t5) * 100, 3), '\t', round(cnt3 / t3 * 100, 3), '\t', round(cnt4 / t4 * 100, 3), '\t', round(cnt5 / t5 * 100, 3), '\t')
